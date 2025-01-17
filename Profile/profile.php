62c6a1571285a4a7af9d1799c7f2d2f114c2327a
Andy ATOHOUN <andymfrd02@gmail.com>
2025-01-17 20:15:43 +0100

    Ajout des champs bio et photo dans l'insertion

--- Profile/profile.php
+++ Profile/profile.php
@@ -10,19 +10,24 @@
         private $picture;
 
 
-        private function __construct($pseudo, $email, $password, $birthday){
+        private function __construct($pseudo, $email, $password, $birthday, $biography, $picture){
             $this->pseudo = $pseudo;
             $this->email = $email;
             $this->password = $password;
             $this->birthday = $birthday;
+            $this->biography = $biography;
+            $this->picture = $picture;
 
             $this->pdo = new Database("localhost", "root", "");
             $connect = $this->pdo->getConnect();
 
             try{
-                $query = "INSERT INTO User(pseudo, email, password, birthday) VALUES (? ? ? ?)";
+                $query = "INSERT INTO User(pseudo, email, password, birthday, Biography, picture) VALUES (? ? ? ? ? ?)";
                 $exec = $connect->prepare($query);
-                $exec->execute([$this->pseudo, $this->email, password_hash($this->password, PASSWORD_DEFAULT), $this->birthday]);
+                $exec->execute([$this->pseudo, $this->email, password_hash($this->password, PASSWORD_DEFAULT),
+                $this->birthday,
+                $this->biography,
+                $this->picture]);
                 
                 echo "Un nouvel utilisateur ajouté";
 
