a9b62184c34ea90a600f47631ac7e7768c66b91c
Andy ATOHOUN <andymfrd02@gmail.com>
2025-01-17 21:26:37 +0100

    Ajout de la fonctionnalité de récupération des informations de l'utilisateur

--- Profile/profile.php
+++ Profile/profile.php
@@ -18,6 +18,25 @@
             $this->picture = $picture;
         }
 
+        public function get($id){
+            $this->pdo = new Database("localhost", "root", "");
+            $connect = $this->pdo->getConnect();
+            
+            $values = null;
+
+            try{
+                $query = "SELECT * FROM User WHERE id = ?";
+                $exec = $connect->prepare($query);
+                $exec->execute([$id]);
+
+                $values = $exec->fetchAll();
+            }catch(PDOException $exception){
+                echo "La récupération des informations de l'utilisateur n'a pas fonctionné : ". $exception;
+            }
+            
+            return $values;
+        }
+
         public function __construct($pseudo, $email, $password, $birthday, $biography, $picture){
             $this->setValues($pseudo, $email, $password, $birthday, $biography, $picture);
 
@@ -50,13 +69,14 @@
             try{
                 $query = "UPDATE User SET pseudo = ?, email = ?, password = ?, birthday = ?, biography = ?,
                     picture = ?
-                    WHERE id = {$id}";
+                    WHERE id = ?";
                 
                 $exec = $connect->prepare($query);
                 $exec->execute([$this->pseudo, $this->email, password_hash($this->password, PASSWORD_DEFAULT),
                 $this->birthday,
                 $this->biography,
-                $this->picture]);
+                $this->picture],
+                $id);
 
                 echo "Modification réalisée avec succès";
 
