f7c57e4af300e9f6ddd10b05712cf8a001529bbd
Andy ATOHOUN <andymfrd02@gmail.com>
2025-01-17 20:53:35 +0100

    Ajout de la fonctionnalité de modification des informations du profil

--- Profile/profile.php
+++ Profile/profile.php
@@ -9,14 +9,17 @@
         private $biography;
         private $picture;
 
-
-        private function __construct($pseudo, $email, $password, $birthday, $biography, $picture){
+        public function setValues($pseudo, $email, $password, $birthday, $biography = null, $picture = null){
             $this->pseudo = $pseudo;
             $this->email = $email;
             $this->password = $password;
             $this->birthday = $birthday;
             $this->biography = $biography;
             $this->picture = $picture;
+        }
+
+        public function __construct($pseudo, $email, $password, $birthday, $biography, $picture){
+            $this->setValues($pseudo, $email, $password, $birthday, $biography, $picture);
 
             $this->pdo = new Database("localhost", "root", "");
             $connect = $this->pdo->getConnect();
@@ -33,6 +36,32 @@
 
             }catch(PDOException $exception){
                 echo "L'insertion de l'utilisateur n'a pas fonctionné : ". $exception->getMessage();
+            }
+
+            $this->pdo = null;
+        }
+
+        
+
+        public function update($id){
+            $this->pdo = new Database("localhost", "root", "");
+            $connect = $this->pdo->getConnect();
+
+            try{
+                $query = "UPDATE User SET pseudo = ?, email = ?, password = ?, birthday = ?, biography = ?,
+                    picture = ?
+                    WHERE id = {$id}";
+                
+                $exec = $connect->prepare($query);
+                $exec->execute([$this->pseudo, $this->email, password_hash($this->password, PASSWORD_DEFAULT),
+                $this->birthday,
+                $this->biography,
+                $this->picture]);
+
+                echo "Modification réalisée avec succès";
+
+            }catch(PDOException $exception){
+                echo "La modification des informations de l'utilisateur n'a pas fonctionné : ". $exception->getMessage();
             }
 
             $this->pdo = null;
